ac1d53cfbe2dfab263667485998f538319c73629
moon-three <moondal366@gmail.com>
2024-12-23 06:55:56 +0900

    [Bronze V] Title: A-B, Time: 164 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		String value = sc.nextLine();
+		int num1 = Integer.parseInt(value.split(" ")[0]);
+		int num2 = Integer.parseInt(value.split(" ")[1]);
+		
+		System.out.println(num1 - num2);
+		
+		sc.close();
+	}
+}
