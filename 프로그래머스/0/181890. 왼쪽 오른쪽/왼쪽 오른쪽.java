504efd136883ac00bf2a1156e4a2a14632a11b1c
moon-three <moondal366@gmail.com>
2024-12-27 23:33:40 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,39 +1,32 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        String[] answer = {};
+        List<String> list = new ArrayList<>();
         
-        String s = "";
-        int index = 0;
-        String value = "";
-                
-        for(int i = 0; i < str_list.length; i++) {
-        	s= str_list[i];
-        	if(s.equals("r") || s.equals("l"))  {
-        		index = i;
+        for (int i = 0; i < str_list.length; i++) {
+        	if (str_list[i].equals("r")) {
+        		
+        		for (int j = i+1; j < str_list.length; j++) {
+        			list.add(str_list[j]);
+        		}
+        		break;
+        		
+        	}
+        	else if (str_list[i].equals("l")) {
+        		for (int j = 0; j < i; j++) {
+        			list.add(str_list[j]);
+        		}
         		break;
         	}
         }
         
-        if(s.equals("r")) {
-        	for(int i = index + 1; i < str_list.length; i++) {
-        		value += str_list[i];
-        	}
+        String[] answer = new String[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+        	answer[i] = list.get(i);
         }
-        if(s.equals("l")) {
-        	for(int i = 0; i < index; i++) {
-        		value += str_list[i];
-        	}
-        }
-        if(s.equals("")) {
-        	value = "";
-        }
-        
-       answer = new String[value.length()];
-       for(int i = 0; i < value.length(); i++) {
-    	   answer[i] = value.charAt(i) + "";
-       }
         
         return answer;
     }
