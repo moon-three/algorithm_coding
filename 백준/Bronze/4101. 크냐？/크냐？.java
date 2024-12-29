ee835fec4aab3210a7f13db1c171a6e68ed8d014
moon-three <moondal366@gmail.com>
2024-12-29 23:52:23 +0900

    [Bronze V] Title: 크냐?, Time: 188 ms, Memory: 17896 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+   public static void main(String[] args) {
+      
+      Scanner sc = new Scanner(System.in);
+      
+      while (true) {
+         
+         int N = sc.nextInt();
+         int M = sc.nextInt();
+         
+         if (N == 0 && M == 0) break;
+         
+         if (N > M) System.out.println("Yes");
+         else System.out.println("No");
+         
+      }
+      
+      sc.close();
+      
+   }
+}
