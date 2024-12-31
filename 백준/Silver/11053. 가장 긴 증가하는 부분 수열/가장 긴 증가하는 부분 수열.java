b10856f75a56c48b6bc7fbd0f5ade228a9e37d07
moon-three <moondal366@gmail.com>
2024-12-31 16:13:22 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.println(getLength(N, arr));
+		
+		br.close();
+	}
+
+	public static int getLength(int N, int[] arr) {
+		List<Integer> list = new ArrayList<>();
+		
+		for(int num : arr) {
+			int idx = Collections.binarySearch(list, num);
+			if(idx < 0) idx = -(idx + 1);
+			if(idx == list.size()) list.add(num);
+			else list.set(idx, num);
+		}
+		
+		return list.size();
+	}
+}
