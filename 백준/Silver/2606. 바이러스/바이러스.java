e8200322eabf7a6b138713961a764312858f9cbc
moon-three <moondal366@gmail.com>
2025-01-05 03:53:56 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());	// 총 컴퓨터 수
+		int M = Integer.parseInt(br.readLine());	// 연결된 컴퓨터 쌍의 수
+		
+		HashMap<Integer, Set<Integer>> hm = new HashMap<>();	// key에 컴퓨터번호, value(set)에 연결된 컴퓨터들을 담는다
+		
+		for(int i = 1; i <= N; i++) {	
+			hm.put(i, new HashSet<Integer>());
+		}
+		
+		for(int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+				int com1 = Integer.parseInt(st.nextToken());
+				int com2 = Integer.parseInt(st.nextToken());
+			
+				hm.get(com1).add(com2);
+				hm.get(com2).add(com1);
+		}
+		
+		boolean[] visited = new boolean[N + 1];
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(1);
+		
+		Set<Integer> virus = new HashSet<Integer>();
+		virus.add(1);
+		
+		while(!queue.isEmpty()) {
+			int i = queue.poll();
+			visited[1] = true;
+			
+			for(int key : hm.get(i)) {
+//				System.out.println("key : " + key);
+//				System.out.println(visited[key]);
+				if (!visited[key]) queue.add(key);
+				visited[key] = true;
+				virus.add(key);
+			}
+//			System.out.println("set : " + virus);
+//			System.out.println();
+		}
+		
+		System.out.println(virus.size() - 1);
+		
+	}
+}
