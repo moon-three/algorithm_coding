ada1cb55e0d86034b7a35e344d92aed3a0039733
moon-three <moondal366@gmail.com>
2025-01-08 04:32:17 +0900

    [Bronze II] Title: 단어의 개수, Time: 280 ms, Memory: 52860 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		String[] arr = br.readLine().trim().split(" ");
+		
+		if(arr[0].equals("")) System.out.println(0);
+		else 			System.out.println(arr.length);
+		
+	}
+}
