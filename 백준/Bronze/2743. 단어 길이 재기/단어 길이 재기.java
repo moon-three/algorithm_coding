5c67d2b2c59cc0db39ad0e18b717bfdc176f666d
moon-three <moondal366@gmail.com>
2025-01-15 04:42:28 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String word = sc.nextLine();
+		System.out.println(word.length());
+		
+		sc.close();
+	}
+}
