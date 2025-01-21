b8c75fd873091ff0f45111b5881a7faf5f0922d4
moon-three <moondal366@gmail.com>
2025-01-22 03:28:33 +0900

    [Gold V] Title: 포스택, Time: 336 ms, Memory: 27840 KB -BaekjoonHub

--- 백준/Gold/25556. 포스택/포스택.java
+++ 백준/Gold/25556. 포스택/포스택.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,29 +15,30 @@ public class Main {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int[] arr = new int[N];
 		List<Stack<Integer>> stackList = new LinkedList<>();		// 4개의 스택을 담을 배열 선언
 		
 		for(int i = 0; i < 4; i++) {
 			stackList.add(new Stack<Integer>());		// 각각의 스택 초기화
 		}
 		
-		int num = 0;
+		boolean flag = true;
 		
 		for(int i = 0; i < N; i++) {
-			num = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
 			
 			if(stackList.get(0).isEmpty() || stackList.get(0).peek() < num) stackList.get(0).push(num);
 			else if(stackList.get(1).isEmpty() || stackList.get(1).peek() < num) stackList.get(1).push(num);
 			else if(stackList.get(2).isEmpty() || stackList.get(2).peek() < num) stackList.get(2).push(num);
 			else if(stackList.get(3).isEmpty() || stackList.get(3).peek() < num) stackList.get(3).push(num);
 			else {
-				System.out.println("NO");
+				flag = false;
 				break;
 			}
 			
-			if(i == N - 1) System.out.println("YES");
 		}
+		
+		if(flag) System.out.println("YES");
+		else System.out.println("NO");
 		
 		br.close();
 	}
