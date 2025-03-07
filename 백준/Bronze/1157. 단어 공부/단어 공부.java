fd57f06ecf4acb8dbb5bebb26fdd1efe3d1aa044
moon-three <moondal366@gmail.com>
2025-03-07 15:00:19 +0900

    [Bronze I] Title: 단어 공부, Time: 252 ms, Memory: 21172 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String word = br.readLine();
+		word = word.toUpperCase();
+		int[] arr = new int['Z' - 'A' + 1];
+		boolean isOne = true;
+		int idx = 0;
+		int maxCnt = 0;
+		
+		for(int i = 0; i < word.length(); i++) {
+			char ch =  word.charAt(i);
+			arr[ch - 'A']++;
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(maxCnt > 0 && maxCnt == arr[i]) {
+				isOne = false;
+			}
+			if(maxCnt < arr[i]) {
+				isOne = true;
+				maxCnt = arr[i];
+				idx = i;
+			}
+		}
+		
+		if(isOne) {
+			System.out.println((char)(idx + 'A'));
+		} else {
+			System.out.println("?");
+		}
+		
+		br.close();
+	}
+}
