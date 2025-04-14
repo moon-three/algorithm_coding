5e5773702cd1621379bf9edfc1329bfe1dd1e205
moon-three <moondal366@gmail.com>
2025-04-14 21:07:02 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16560 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int left = 0; 
+		int right = N - 1;
+		int cnt = 0;
+		
+		while (left < right) {
+			
+			if(arr[left] + arr[right] == M) {
+				cnt++;
+				left++;
+				right--;
+			} else if(arr[left] + arr[right] > M) {
+				right--;
+			} else {
+				left++;
+			}
+			
+		}
+	
+		System.out.println(cnt);
+		br.close();
+	}
+}
