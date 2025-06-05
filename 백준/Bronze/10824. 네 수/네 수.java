00e9657c6df865173df3725bc559900d7561cf67
moon-three <moondal366@gmail.com>
2025-06-06 03:37:16 +0900

    [Bronze III] Title: 네 수, Time: 112 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String AB = st.nextToken() + st.nextToken();
+		String CD = st.nextToken() + st.nextToken();
+		
+		System.out.println(Long.parseLong(AB) + Long.parseLong(CD));
+		br.close();
+		
+	}
+}
