ab3d5bf4e78dbf539f09ce1d15b797636962d7e4
moon-three <moondal366@gmail.com>
2025-08-05 00:57:55 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        String s = String.valueOf(x);
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++) {
+            sum += s.charAt(i) - '0';
+        }
+        if(x % sum != 0) {
+            answer = false;
+        } 
+        
+        return answer;
+    }
+}
