ec85452602dbc46652fec73136e10ba285478de6
moon-three <moondal366@gmail.com>
2025-08-22 02:10:54 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.51 ms, Memory: 93.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {-1};
+        
+        if(arr.length == 1) {
+            return answer;
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        
+        answer = new int[arr.length - 1];
+        int idx = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] != min) {
+                answer[idx++] = arr[i];
+            }        
+        }
+        
+        return answer;
+    }
+}
