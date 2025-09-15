15a3b8303dc6d097bec4cd4cf1df1d04956172c4
moon-three <moondal366@gmail.com>
2025-09-15 21:23:11 +0900

    [Silver I] Title: 포도주 시식, Time: 128 ms, Memory: 15008 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+
+    static int[] wine;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        wine = new int[N+1];
+        dp = new int[N+1];
+
+        for(int i = 1; i <= N; i++) {
+            wine[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = wine[1];
+        if(N >= 2) {
+            dp[2] = dp[1] + wine[2];
+        }
+
+        for(int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i]));
+        }
+
+        System.out.println(dp[N]);
+        br.close();
+    }
+}
