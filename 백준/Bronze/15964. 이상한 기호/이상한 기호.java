87d2b733838eba9956ee57637fefa734e15c7cff
moon-three <moondal366@gmail.com>
2025-10-02 18:57:40 +0900

    [Bronze V] Title: 이상한 기호, Time: 168 ms, Memory: 17648 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        
+        System.out.println((A+B)*(A-B));
+        sc.close();
+    }
+}
