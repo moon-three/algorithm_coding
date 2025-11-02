600d396f726a7220ff2c367791712822c714ba2f
moon-three <moondal366@gmail.com>
2025-11-02 20:46:00 +0900

    [level 0] Title: 할 일 목록, Time: 12.48 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        String[] answer = {};
+        String s = "";
+        
+        for(int  i = 0; i < todo_list.length; i++) {
+            if(finished[i] == false) {
+                s += todo_list[i] + " ";
+            } 
+        }
+        answer = s.split(" ");
+        return answer;
+    }
+}
