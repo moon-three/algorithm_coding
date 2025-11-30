d51ee2b607d2dfcec859e6c24f89c2736354fc74
moon-three <moondal366@gmail.com>
2025-11-30 20:18:45 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1216 ms, Memory: 108308 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length -1; i >= 0; i--) {
+            sb.append(arr[i]).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
