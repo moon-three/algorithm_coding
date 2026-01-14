2935c51f79325e71230c41485e82c0bd11423548
moon-three <moondal366@gmail.com>
2026-01-14 19:17:35 +0900

    [Silver I] Title: 스타트와 링크, Time: 396 ms, Memory: 19020 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[][] table;
+    static boolean[] isTeam;
+    static int minDiffer = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        table = new int[N + 1][N + 1];
+        isTeam = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                table[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0, 1);
+        System.out.println(minDiffer);
+        br.close();
+    }
+
+    static void dfs(int cnt, int start) {
+        if (cnt == N / 2) {
+            // 그 팀의 점수를 구하고 비교
+            int startTeam = 0;
+            int linkTeam = 0;
+            for (int i = 1; i <= N; i++) {
+                for (int j = i + 1; j <= N; j++) {
+                    if (isTeam[i] && isTeam[j]) {
+                        startTeam += table[i][j] + table[j][i];
+                    } else if (!isTeam[i] && !isTeam[j]) {
+                        linkTeam += table[i][j] + table[j][i];
+                    }
+                }
+            }
+            minDiffer = Math.min(minDiffer, Math.abs(startTeam - linkTeam));
+            return;
+        }
+
+        // 팀을 나누고
+        for (int i = start; i <= N; i++) {
+            if (!isTeam[i]) {
+                isTeam[i] = true;
+                dfs(cnt + 1, i + 1);
+                isTeam[i] = false;
+            }
+        }
+    }
+}
