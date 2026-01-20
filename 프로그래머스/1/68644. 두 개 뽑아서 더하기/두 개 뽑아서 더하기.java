10b8882e1ed11c15001b8ee0b94947a8fc32093a
moon-three <moondal366@gmail.com>
2026-01-20 12:55:07 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.44 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = i+1; j < numbers.length; j++) {
+                int sum = numbers[i] + numbers[j];
+                set.add(sum);
+            }
+        }
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        
+        answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
