1153b1473b84ce61718dffe17036c693a61e4e53
moon-three <moondal366@gmail.com>
2026-02-17 20:19:33 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board) {
+        int answer = 0;
+        int n = board.length;
+        
+        int[][] dirs = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}
+        };
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == 1) {
+                    for(int[] dir : dirs) {
+                        int next_i = i + dir[0];
+                        int next_j = j + dir[1];
+                        
+                        if(next_i < 0 || next_i >= n || next_j < 0 || next_j >= n) continue;
+                        if(board[next_i][next_j] == 1) continue;
+                        
+                        board[next_i][next_j] = 2;
+                    }
+                }
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == 0) answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
