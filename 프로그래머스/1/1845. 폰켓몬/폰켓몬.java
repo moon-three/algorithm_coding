c486a502c9a452ef43361134dd7bcc71a17ed1d0
moon-three <moondal366@gmail.com>
2026-02-17 22:35:08 +0900

    [level 1] Title: 폰켓몬, Time: 5.50 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,33 +1,17 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        // set 개수가 더 크면 배열의 반, set 개수가 더 작으면 set 개수
-        
-//      List list = Arrays.asList(nums); // int[] 로 들어와서 안되는건가..
-//      Set set = new HashSet(list);
-        
-//      if(set.size() >= (nums.length / 2)) {
-//          answer = nums.length / 2;
-//      } else answer = set.size();
-        
-        // --> primitive 배열은 WrapperClass List 로 바로 변환되지 않는다
-        // --> set.add로 담아줘야해 ...
-        
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int num : nums) {
-            set.add(num);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
         
-        int half = nums.length / 2;    // 배열 길이의 절반
-        int kind = set.size();         // 종류의 개수 
-        answer = half < kind ? half : kind;
+        if(nums.length / 2 > map.size()) {
+            return map.size();
+        }
         
-        return answer;
+        return nums.length / 2;
     }
 }
