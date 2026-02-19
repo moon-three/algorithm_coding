ab68edeeaa9dee6d72b48186e374cc0ec2e0603c
moon-three <moondal366@gmail.com>
2026-02-19 22:51:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 286.84 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,15 @@
 import java.util.*;
 
 class Solution {
-     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<participant.length; i++) {
-            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+    public String solution(String[] participant, String[] completion) {
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for(int i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i])) return participant[i];
         }
-        for(int i=0; i<completion.length; i++) {
-            map.put(completion[i], map.get(completion[i])-1);
-        }
-        for(String key : map.keySet()) {
-            if(map.get(key) == 1) {
-                return key;
-            }
-        }
-        return "";
+        
+        return participant[participant.length - 1];
     }
 }
