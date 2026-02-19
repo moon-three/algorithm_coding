1594aa9c79bef3768e7c7a6a7e8912a32739a820
moon-three <moondal366@gmail.com>
2026-02-19 22:13:09 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,15 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
+        int answer = 0;
         
-        for(String[] cloth : clothes) {
-            map.put(cloth[1], map.getOrDefault(cloth[1], 1) + 1);
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        for(int i = 0; i < clothes.length; i++) {
+            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 1) + 1);
         }
         
         int result = 1;
-        for(String key : map.keySet()) {
-            result *= map.get(key);
+        for(Map.Entry<String, Integer> entry : hm.entrySet()) {
+            result *= entry.getValue();
         }
         
         return result-1;
