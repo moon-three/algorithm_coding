501772899df88e04e24dbfcb132085a62e473eec
moon-three <moondal366@gmail.com>
2026-02-19 22:11:43 +0900

    [level 2] Title: 의상, Time: 0.17 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,18 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
+        Map<String, Integer> map = new HashMap<>();
         
-        HashMap<String, Integer> hm = new HashMap<>();
-        
-        for(int i = 0; i < clothes.length; i++) {
-            // 각각 의상 종류에 해당하는 옷을 안 착용하는 경우를 위해 default를 1로 잡음
-            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 1) + 1);
+        for(String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 1) + 1);
         }
         
         int result = 1;
-        for(Map.Entry<String, Integer> entry : hm.entrySet()) {
-            result *= entry.getValue();
+        for(String key : map.keySet()) {
+            result *= map.get(key);
         }
         
         return result-1;
