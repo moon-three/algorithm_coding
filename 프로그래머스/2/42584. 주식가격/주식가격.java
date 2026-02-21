f6e6913e910b5c5ae853ce83633b3d84c76484be
moon-three <moondal366@gmail.com>
2026-02-21 15:20:54 +0900

    [level 2] Title: 주식가격, Time: 14.61 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,30 +1,19 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-
-        Queue<Integer> queue = new LinkedList<>();
-
+        
         for(int i = 0; i < prices.length; i++) {
-            queue.add(prices[i]);
-        }
-
-        int i = 0;
-        while(!queue.isEmpty()) {
-            int cur = queue.poll();
+            int cur = prices[i];
             int cnt = 0;
-
-            for(int next : queue) {
+            for(int j = i + 1; j < prices.length; j++) {
                 cnt++;
-                if(next < cur) {
+                if(prices[j] < cur) {
                     break;
                 }
             }
-            answer[i++] = cnt;
+            answer[i] = cnt;
         }
+
         return answer;
     }
 }
