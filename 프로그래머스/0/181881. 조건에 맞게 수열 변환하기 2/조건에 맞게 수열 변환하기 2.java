9b5c77bd20f3e4c73f808c0c879408f85355804a
moon-three <moondal366@gmail.com>
2026-03-02 00:03:58 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 81.40 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -5,21 +5,18 @@ class Solution {
     	int answer = 0;
 
 		while (true) {
-			int[] copy = new int[arr.length];		// 비교를 하기 위한 배열
+			int[] copy = new int[arr.length];
 			
 			for (int i = 0; i < arr.length; i++) {
-				copy[i] = arr[i];					// 미리 값을 저장
+				copy[i] = arr[i];
 				if (arr[i] >= 50 && arr[i] % 2 == 0) 		arr[i] /= 2;	
 				else if (arr[i] < 50 && arr[i] % 2 != 0)	arr[i] = arr[i] * 2 + 1;
 			}		
 			answer += 1;
-			
-//			System.out.println("copy : " + Arrays.toString(copy));
-//			System.out.println("arr : " + Arrays.toString(arr));
-			
-			if(Arrays.equals(copy, arr)) {	// 이전 배열과 같으면 반복문 종료 (비교 시 Arrays.equals() 사용)
-				answer -= 1;	// 1 감소 후
-				break; 		    // 종료
+
+			if(Arrays.equals(copy, arr)) {
+				answer -= 1;
+				break;
 			}
 		}     
       return answer;
