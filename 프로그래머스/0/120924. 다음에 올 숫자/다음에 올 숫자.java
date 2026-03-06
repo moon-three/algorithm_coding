41ceffe626fda579ee84a07a6ba1dd5ea4209889
moon-three <moondal366@gmail.com>
2026-03-07 00:47:17 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,14 +1,10 @@
 class Solution {
     public int solution(int[] common) {
-        int answer = 0;
-        
         int diff1 = common[1] - common[0];
         int diff2 = common[2] - common[1];
         
         int last = common[common.length-1];
-        if(diff1 == diff2) answer = last + diff1;
-        else answer = last * (diff2/diff1);
-        
-        return answer;
+        if(diff1 == diff2) return last + diff1;
+        else return last * (diff2/diff1);
     }
 }
