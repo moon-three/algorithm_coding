9abea1a8f430340273ad0c3ec1be8d224be8e4fe
moon-three <moondal366@gmail.com>
2026-03-08 16:19:20 +0900

    [level 1] Title: 기사단원의 무기, Time: 8.21 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -3,15 +3,10 @@ class Solution {
         int answer = 0;
         
         int[] arr = new int[number+1];
-        for(int i = 1; i <= number; i++) {
-            int cnt = 0;
-            for(int j = 1; j * j <= i; j++) {
-                if(i % j == 0) {
-                    if(j * j == i) cnt += 1;
-                    else cnt += 2;
-                }
-            }
-            arr[i] = cnt;
+        for(int i = 1; i <= number; i++) {  // i가 약수
+           for(int j = i; j <= number; j+=i) {  // 배수인 수에 +1
+               arr[j]++;
+           }
         }
         
         for(int i = 1; i <= number; i++) {
