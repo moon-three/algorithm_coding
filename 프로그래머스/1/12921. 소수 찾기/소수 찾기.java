468051b656dc87193e7e91ba5e24906b7775c1eb
moon-three <moondal366@gmail.com>
2026-03-09 22:49:30 +0900

    [level 1] Title: 소수 찾기, Time: 17.98 ms, Memory: 57.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n+1];
+        
+        for(int i = 2; i <= n; i++) {
+            if(arr[i] == 1) continue;
+            for(int j = i; j <= n; j+=i) {
+                arr[j] = 1; 
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
