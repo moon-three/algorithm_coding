2c1fea54097088c0ad945cd98fc43b66c12fbfc2
moon-three <moondal366@gmail.com>
2026-03-26 22:43:24 +0900

    [Bronze V] Title: 오늘 날짜, Time: 160 ms, Memory: 17268 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+
+    }
+}
