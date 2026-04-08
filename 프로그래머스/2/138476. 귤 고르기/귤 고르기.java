8b9009f2ed5739d43e0387a489745379e7cd3110
moon-three <moondal366@gmail.com>
2026-04-08 11:39:16 +0900

    [level 2] Title: 귤 고르기, Time: 67.96 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < tangerine.length; i++) {
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        }
+        
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((a, b) -> b.getValue() - a.getValue());
+  
+        for(Map.Entry<Integer, Integer> entry : list) {
+            if(k <= 0) break;
+            
+            k -= entry.getValue();
+            answer++;
+        }
+        
+        return answer;
+    }
+}
