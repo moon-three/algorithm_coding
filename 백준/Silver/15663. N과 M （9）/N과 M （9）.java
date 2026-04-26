c046e92a26944d21972dadedd3c78a9804b2677b
moon-three <moondal366@gmail.com>
2026-04-26 23:05:13 +0900

    [Silver II] Title: N과 M (9), Time: 176 ms, Memory: 21424 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        visited = new boolean[N];
+        result = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0);
+        System.out.println(sb);
+        br.close();
+    }
+
+    public static void dfs(int depth) {
+        if(depth == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int last = -1;
+        for(int i = 0; i < N; i++) {
+            if(last == arr[i]) continue;
+            if(visited[i]) continue;
+
+            result[depth] = arr[i];
+            visited[i] = true;
+            last = arr[i];
+
+            dfs(depth + 1);
+            visited[i] = false;
+
+        }
+    }
+
+}
