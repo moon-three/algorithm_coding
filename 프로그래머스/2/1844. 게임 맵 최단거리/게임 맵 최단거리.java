f2a23d9f2e55d194c6dabc973d10e2146901cb67
moon-three <moondal366@gmail.com>
2026-04-27 09:59:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.77 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,15 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
+        int[][] dirs = new int[][] {
+            {-1, 0}, {1, 0}, {0, -1}, {0, 1}  
+        };
         int N = maps.length;
         int M = maps[0].length;
-        int[][] dirs = {
-            {-1, 0}, {1, 0}, {0, -1}, {0, 1}
-        };
+        
         int[][] dist = new int[N][M];
-        for(int i = 0; i < dist.length; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
         
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{0,0});
@@ -24,18 +22,15 @@ class Solution {
             for(int[] dir : dirs) {
                 int next_n = n + dir[0];
                 int next_m = m + dir[1];
-
-                if(next_n < 0 || next_m < 0 || next_n >= N || next_m >= M) continue;
-
-                if(dist[next_n][next_m] != Integer.MAX_VALUE) continue;
-
-                if(maps[next_n][next_m] == 1) {
-                    dist[next_n][next_m] = dist[n][m] + 1;
-                    queue.add(new int[] {next_n, next_m});
-                }
-            }
+                
+                if(next_n < 0 || next_n >= N || next_m < 0 || next_m >= M) continue;
+                if(maps[next_n][next_m] == 0) continue;
+                if(dist[next_n][next_m] != 0) continue;
+                
+                dist[next_n][next_m] = dist[n][m] + 1;
+                queue.add(new int[]{next_n, next_m});
+            }   
         }
-        
-        return dist[N-1][M-1] == Integer.MAX_VALUE ? -1 : dist[N-1][M-1];
+        return dist[N-1][M-1] == 0 ?-1 : dist[N-1][M-1];
     }
 }
