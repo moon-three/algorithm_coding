c161a589aba9ef16a2211e5d5e95aaaae827acb6
moon-three <moondal366@gmail.com>
2026-05-03 22:43:55 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,16 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] arr) {
-        int answer = arr[0];
-        
-        for(int i = 1; i < arr.length; i++) {
-            answer = answer * arr[i] / gcd(answer, arr[i]);
+        Queue<Integer> queue = new LinkedList<>();
+        for(int n : arr) {
+            queue.add(n);
         }
         
-        return answer;
+        while(queue.size() >= 2) {
+            int n1 = queue.poll();
+            int n2 = queue.poll();
+            
+            int lcm = n1 * (n2 / gcd(n1, n2));
+            
+            queue.add(lcm);
+        }
+        
+        return queue.poll();
     }
     
     public int gcd(int a, int b) {
-        if(b == 0) return a;
+        if(b == 0) {
+            return a;
+        }
         return gcd(b, a % b);
     }
+    
 }
