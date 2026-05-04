27e0245d05a54c436e5964034d4119ba6648e713
moon-three <moondal366@gmail.com>
2026-05-04 22:15:53 +0900

    [level 1] Title: 둘만의 암호, Time: 13.13 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        
+        Set<Integer> skips = new HashSet<>();
+        
+        for(int i = 0; i < skip.length(); i++) {
+            int value = skip.charAt(i) - 'a';
+            skips.add(value);
+        }
+        
+        for(int i = 0; i < s.length(); i++) {
+            int target = s.charAt(i) - 'a';
+           
+            for(int j = 1; j <= index; j++) {
+                target++;
+                if(target >= 26) target %= 26;
+                if(skips.contains(target)) {
+                    j--;
+                }
+            }
+            answer += (char)('a' + target);
+        }
+        return answer;
+    }
+}
