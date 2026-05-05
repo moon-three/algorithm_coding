cb33868d8635be831bd7b433358b3168e0bcc24d
moon-three <moondal366@gmail.com>
2026-05-05 22:13:25 +0900

    [level 1] Title: 문자열 나누기, Time: 1.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -2,26 +2,22 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        char[] chs = s.toCharArray();
-        
-        char value = chs[0];
+        char value = 0;
         int same = 0;
         int other = 0;
-        boolean isEqual = false;
-        for(int i = 0; i < chs.length; i++) {
-            if(isEqual) {
-                answer++;
-                value = chs[i];
-                same = 0;
-                other = 0;
-                isEqual = false;
-            }
-            if(chs[i] == value) same++;
+        for(int i = 0; i < s.length(); i++) {
+            if(same == 0) value = s.charAt(i);
+            
+            if(value == s.charAt(i)) same++;
             else other++;
             
-            if(same == other) isEqual = true;
-        }
-        answer++;   // 마지막에 하나
+            if(same == other) {
+                answer++;
+                same = 0;
+                other = 0;
+            }
+        } 
+        if(same != 0) answer++;
         
         return answer;
     }
