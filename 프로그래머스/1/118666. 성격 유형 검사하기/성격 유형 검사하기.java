ffbef68180d9ccf6e0917528ed8426442c0771c3
moon-three <moondal366@gmail.com>
2026-05-08 22:13:31 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 15.28 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < choices.length; i++) {
+            String s = survey[i];
+            int choice = choices[i];
+            if(choice == 4) continue;
+            char target = 0;
+            if(choice < 4) {
+                target = s.charAt(0);
+            } else {
+                target = s.charAt(1);
+            }
+            map.put(target, map.getOrDefault(target, 0) + Math.abs(4-choice));
+        }
+        
+        System.out.println(map);
+        
+        String result  = "";
+        result += map.getOrDefault('R', 0) >= map.getOrDefault('T', 0) ? 'R' : 'T';
+        result += map.getOrDefault('C', 0) >= map.getOrDefault('F', 0) ? 'C' : 'F';
+        result += map.getOrDefault('J', 0) >= map.getOrDefault('M', 0) ? 'J' : 'M';
+        result += map.getOrDefault('A', 0) >= map.getOrDefault('N', 0) ? 'A' : 'N';
+        
+        return result;
+    }
+}
