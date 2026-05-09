31e9fe005d3954f105dfc814bf7968debdab3691
moon-three <moondal366@gmail.com>
2026-05-10 00:06:29 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.40 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         
         for(int i = 1; i <= n; i++) {
             if(i % 2 != 0) {
-                answer += "수";
+                sb.append("수");
             } else {
-                answer += "박";
+                sb.append("박");
             }          
         }
-        return answer;
+        return sb.toString();
     }
 }
