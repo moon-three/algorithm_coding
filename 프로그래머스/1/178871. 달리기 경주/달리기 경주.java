315a2a22cc7191dba2e2644f34cd55ff709cf0ed
moon-three <moondal366@gmail.com>
2026-05-12 17:22:53 +0900

    [level 1] Title: 달리기 경주, Time: 291.71 ms, Memory: 357 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,29 +2,22 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] grades = new String[players.length + 1];
         Map<String, Integer> map = new HashMap<>();
-        
-        for(int i = 1; i < grades.length; i++) {
-            grades[i] = players[i-1];
-            map.put(players[i-1], i);
-        }       
+    
+        for(int i = 0; i < players.length; i++) {
+            map.put(players[i], i + 1);
+        }
         
         for(int i = 0; i < callings.length; i++) {
-            int idx = map.get(callings[i]);
+            int idx = map.get(callings[i]) - 1;
             
-            grades[idx] = grades[idx-1]; 
-            grades[idx-1] = callings[i];
+            players[idx] = players[idx-1]; 
+            players[idx-1] = callings[i];
             
-            map.put(grades[idx], idx);
-            map.put(callings[i], idx-1);
-        }
-
-        String[] answer = new String[players.length];
-        for(int i = 1; i < grades.length; i++) {
-            answer[i-1] = grades[i];
+            map.put(players[idx], idx + 1);
+            map.put(callings[i], idx);
         }
         
-        return answer;
+        return players;
     }
 }
