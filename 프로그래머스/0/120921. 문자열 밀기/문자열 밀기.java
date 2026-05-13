fd2412fd7eb2e0f23fb2438c8d1bede1f794b0ca
moon-three <moondal366@gmail.com>
2026-05-13 22:57:55 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        
+        for(int i = 0; i < B.length(); i++) {
+            if(B.charAt(i) == A.charAt(0)) {
+                boolean result = checkWords(A, B, i);
+                if(result) return i;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public boolean checkWords(String A, String B, int idx) {
+        for(int i = 0; i < A.length(); i++) {
+            int index = idx+i >= B.length() ? (idx+i)%B.length() : idx+i;
+            if(A.charAt(i) != B.charAt(index)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
