b66edb109b7d2a842cdd3a49f306db6172e4920f
moon-three <moondal366@gmail.com>
2026-05-30 00:02:20 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] solution(int[][] score) {
-        int[] answer = new int[score.length];
+        int length = score.length;
+        int[] answer = new int[length];
         
-        double[] avgs = new double[score.length];
-        for(int i = 0; i < score.length; i++) {
+        double[] avgs = new double[length];
+        for(int i = 0; i < length; i++) {
             avgs[i] = (score[i][0] + score[i][1]) / 2.0;
         }
         
-        for(int i = 0; i < avgs.length; i++) {
+        for(int i = 0; i < length; i++) {
             int rank = 1;
-            for(int j = 0; j < avgs.length; j++) {
+            for(int j = 0; j < length; j++) {
                 if(avgs[i] < avgs[j]) {
                     rank++;
                 }
