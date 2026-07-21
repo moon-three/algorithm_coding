21a2e44bfd23b5111473b01a99b2b4d5764cd680
moon-three <moondal366@gmail.com>
2026-07-22 00:00:50 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 3.93 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내림차순으로 배치하기 - 12917 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12917?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12917) 
 
 ### 성능 요약
 
-메모리: 71.8 MB, 시간: 18.15 ms
+메모리: 82.1 MB, 시간: 3.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 27일 21:32:01
+2026년 07월 22일 00:00:46
 
 ### 문제 설명
 
--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -2,19 +2,13 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
+        char[] chs = s.toCharArray();
+        Arrays.sort(chs);
+        
         String answer = "";
         
-        int[] arr = new int[s.length()];
-        
-        for(int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            arr[i] = (int)ch;
-        }
-        
-        Arrays.sort(arr);
-        
-        for(int i = arr.length -1; i >= 0; i--) {
-            answer += (char) arr[i];
+        for(int i = chs.length - 1; i >= 0; i--) {
+            answer += chs[i];
         }
         
         return answer;
