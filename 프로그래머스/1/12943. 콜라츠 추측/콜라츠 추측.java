8f5c0d65f8327962eaca309281b7703b8973afd6
moon-three <moondal366@gmail.com>
2026-08-01 21:55:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,22 +1,19 @@
 class Solution {
-    public int solution(int num) {    
-        if(num == 1) {
-            return 0;
-        }
-        
+    public int solution(int num) {
+        long n = num;
         int cnt = 0;
         
-        while(num > 1) {
-            if(cnt >= 500) return -1;
-            
-            if(num % 2 == 0) {
-                num /= 2;
+        while(n != 1) {
+            if(n % 2 == 0) {
+                n /= 2;
             } else {
-                num = num * 3 + 1;
+                n = n * 3 + 1;
             }
             cnt++;
+            
+            if(cnt > 500) return -1;
         }
-    
-        return num==1? cnt:-1;
+        
+        return cnt;
     }
 }
