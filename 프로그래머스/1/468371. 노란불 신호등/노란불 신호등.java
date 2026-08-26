7ac702bce14f6ac1f8c79ca768c49275c0c624e6
moon-three <moondal366@gmail.com>
2026-08-26 23:09:20 +0900

    [level 1] Title: 노란불 신호등, Time: 8.54 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int solution(int[][] signals) {
+
+        int limit = signals[0][0] + signals[0][1] + signals[0][2];
+        for(int i = 1; i < signals.length; i++) {
+            int period = signals[i][0] + signals[i][1] + signals[i][2];
+            limit = lcm(limit, period);
+        }
+        
+        for(int time = 1; time <= limit; time++) {
+            
+            boolean isYellow = true;
+            
+            for(int i = 0; i < signals.length; i++) {
+                int g = signals[i][0];
+                int y = signals[i][1];
+                int r = signals[i][2];
+                
+                int cycle = g + y + r;
+                int cur = time % cycle;
+                
+                if(!(g < cur && cur <= g + y)) {
+                    isYellow = false;
+                    break;
+                }
+            }
+            
+            if(isYellow) {
+                return time;
+            }
+            
+        }
+        
+        return -1;
+    }
+    
+    int gcd(int a, int b) {
+        while(b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+    
+    int lcm(int a, int b) {
+        return a / gcd(a, b) * b;
+    }
+}
+
+// 3부터 5씩 더함
+// 6부터 7씩 더함
+
+// 5부터 7씩 더함
+// 4부터 7씩 더함
+// 3부터 4씩 더함 
+
