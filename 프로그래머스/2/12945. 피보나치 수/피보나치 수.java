371123236030d593f033c958619fd3883f2f7339
moon-three <moondal366@gmail.com>
2026-09-03 10:45:02 +0900

    [level 2] Title: 피보나치 수, Time: 1.85 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,15 +1,13 @@
 class Solution {
-    static int[] dp;
     public int solution(int n) {
-        return fibonacci(n);
-    }
-    
-    public int fibonacci(int n) {
-        dp = new int[n+1];
-        dp[1] = 1;
+        int[] arr = new int[n+1];
+        
+        arr[1] = 1;
+        
         for(int i = 2; i <= n; i++) {
-            dp[i] = (dp[i-2] % 1234567 + dp[i-1] % 1234567) % 1234567;
+            arr[i] = (arr[i-2] % 1234567) + (arr[i-1] % 1234567);
         }
-        return dp[n];
+        
+        return arr[n] % 1234567;
     }
 }
