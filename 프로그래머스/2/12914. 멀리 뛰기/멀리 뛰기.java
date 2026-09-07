41b1ebc8ba2e65a93f12f982bcdb62d5195747ae
moon-three <moondal366@gmail.com>
2026-09-07 11:43:39 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.08 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,15 +1,19 @@
+import java.util.*;
+
 class Solution {
     public long solution(int n) {
+        
         if(n == 1) return 1;
-    
-        int[] arr = new int[n+1];
-        arr[1] = 1;
-        arr[2] = 2;
+        
+        long[] dp = new long[n+1];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
         for(int i = 3; i <= n; i++) {
-            arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
+            dp[i] = dp[i-1] % 1234567 + dp[i-2] % 1234567;
         }
-        
-        
-        return arr[n];
+
+        return dp[n] % 1234567;
     }
 }
