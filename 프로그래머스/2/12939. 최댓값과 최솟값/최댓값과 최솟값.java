2adde5e4304e03fad964fdf200b5104c30c57590
moon-three <moondal366@gmail.com>
2026-09-08 11:27:24 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.05 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,19 +1,11 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
-        String answer = "";
-       
         String[] sArr = s.split(" ");
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        int[] arr = Arrays.stream(sArr).mapToInt(o -> Integer.parseInt(o)).toArray();
+        Arrays.sort(arr);
         
-        for(int i = 0; i < sArr.length; i++) {
-            int cur = Integer.parseInt(sArr[i]);
-            min = Math.min(min, cur);
-            max = Math.max(max, cur);
-        }
-        
-        answer = min + " " + max;
-        
-        return answer;
+        return arr[0] + " " + arr[arr.length - 1];
     }
 }
