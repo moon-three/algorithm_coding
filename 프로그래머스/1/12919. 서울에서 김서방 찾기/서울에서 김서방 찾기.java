a0f7a4b54091392976b4c787a10023f561d8346f
moon-three <moondal366@gmail.com>
2026-09-14 10:50:48 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.89 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,10 +1,8 @@
 class Solution {
     public String solution(String[] seoul) {
-  
+
         for(int i = 0; i < seoul.length; i++) {
-            if(seoul[i].equals("Kim")) {
-                return "김서방은 " + i + "에 있다";
-            }
+            if(seoul[i].equals("Kim")) return String.format("김서방은 %d에 있다", i);
         }
         
         return "";
