5639040dcd689b749c0bcdf1167328efb4926fab
moon-three <moondal366@gmail.com>
2026-09-14 11:05:23 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -2,12 +2,14 @@ class Solution {
     public String solution(String phone_number) {
         StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i < phone_number.length() - 4; i++) {
+        int point = phone_number.length() - 4;
+        
+        for(int i = 0; i < point; i++) {
             sb.append("*");
         }
         
-        sb.append(phone_number.substring(phone_number.length() - 4));
- 
+        sb.append(phone_number.substring(point));
+        
         return sb.toString();
     }
 }
