4ef59a729ddf6d07b9e1cb653b8b8f15d1eea7c0
moon-three <moondal366@gmail.com>
2026-09-18 12:15:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 22.26 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,12 +1,11 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int[] arr) {
+    public int[] solution(int []arr) {
         List<Integer> list = new ArrayList<>();
-        list.add(arr[0]);
         
-        for(int i = 1; i < arr.length; i++) {
-            if(arr[i-1] == arr[i]) continue;
+        for(int i = 0; i < arr.length; i++) {
+            if(i != 0 && arr[i-1] == arr[i]) continue;
             list.add(arr[i]);
         }
         
