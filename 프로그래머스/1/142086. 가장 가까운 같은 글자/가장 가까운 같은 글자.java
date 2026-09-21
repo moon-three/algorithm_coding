e47147a7f353032eb4359ca62ed9b4127ebffc5b
moon-three <moondal366@gmail.com>
2026-09-22 00:23:00 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.64 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,19 +1,18 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
         
-        HashMap<Character, Integer> hm = new HashMap<>();
-        char[] chs = s.toCharArray();
-        
-        for(int i = 0; i < chs.length; i++) {
-            if(!hm.containsKey(chs[i])) {
-                answer[i] = -1;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)) {
+                answer[i] = i - map.get(ch);
             } else {
-                answer[i] = i - hm.get(chs[i]);
+                answer[i] = -1;
             }
-            hm.put(chs[i], i);
+            map.put(ch, i);
         }
         
         return answer;
