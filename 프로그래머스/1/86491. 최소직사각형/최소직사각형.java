3bea3894ccb640aa934f119c7d9fe02559a00026
moon-three <moondal366@gmail.com>
2026-09-21 15:28:15 +0900

    [level 1] Title: 최소직사각형, Time: 3.15 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,21 +1,19 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    public int solution(int[][] sizes) {
-        int answer = 0;
-
+    public int solution(int[][] sizes) {  
+        int w = 0;
+        int h = 0;
+        
         for(int i = 0; i < sizes.length; i++) {
             Arrays.sort(sizes[i]);
         }
-
-        int maxW = 0, maxH = 0;
+        
         for(int i = 0; i < sizes.length; i++) {
-            maxW = Math.max(maxW, sizes[i][0]);
-            maxH = Math.max(maxH, sizes[i][1]);
+            if(w < sizes[i][0]) w = sizes[i][0];
+            if(h < sizes[i][1]) h = sizes[i][1];
         }
-
-        answer = maxW * maxH;
-
-        return answer;
+        
+        return w * h;
     }
 }
